6291a6174e0540bc7916be9f7ac4383fa538b840
0zd0 <67220210+0zd0@users.noreply.github.com>
2026-04-06 19:26:31 +0400

    feat: get service price type

---
+++ src/Dictionaries/Requests/GetServicePriceTypesRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Onepix\RenovatioSdk\Dictionaries\Requests;
+
+use Onepix\RenovatioSdk\Dictionaries\DTO\ServicePriceType;
+use Onepix\RenovatioSdk\Shared\Requests\BaseRenovatioRequest;
+use Onepix\RenovatioSdk\Shared\Requests\Traits\HasVisibilityFilters;
+use Saloon\Enums\Method;
+
+final class GetServicePriceTypesRequest extends BaseRenovatioRequest
+{
+    use HasVisibilityFilters;
+
+    protected Method $method = Method::POST;
+
+    public function resolveEndpoint(): string
+    {
+        return '/getServicePriceTypes';
+    }
+
+    protected function defaultBody(): array
+    {
+        $payload = $this->getVisibilityPayload();
+
+        unset($payload['show_all'], $payload['source']);
+
+        return $payload;
+    }
+
+    protected function getDtoClass(): string
+    {
+        return 'array<' . ServicePriceType::class . '>';
+    }
+}
